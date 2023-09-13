8e37933d74d0d412d9cbb49dc382df68b1640f3a
Jeferson Costa <jeferson_cta@hotmail.com>
2023-09-13 13:36:30 -0300

    first commit

---
+++ src/routes/create-transcripion.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod"
+import { prisma } from "../lib/prisma";
+import { createReadStream } from 'node:fs'
+import { openai } from "../lib/openai";
+export async function createTranscriptionRoute(app: FastifyInstance) {
+    app.post('/videos/:videoId/transcription', async (request) => {
+        const paramsSchema = z.object({
+            videoId: z.string().uuid(),
+        })
+        const { videoId } = paramsSchema.parse(request.params)
+
+        const bodySchema = z.object({
+            prompt: z.string()
+        })
+
+        const { prompt } = bodySchema.parse(request.body)
+
+        const video = await prisma.video.findFirstOrThrow(
+            {
+                where: {
+                    id: videoId
+                }
+            }
+        )
+        const videoPath = video.path
+        const audioreadStream = createReadStream(videoPath)
+
+        const response = await openai.audio.transcriptions.create({
+            file: audioreadStream,
+            model: 'whisper-1',
+            language: 'pt',
+            response_format: 'json',
+            temperature: 0,
+            prompt
+        })
+
+        const transcription = response.text
+        await prisma.video.update({
+            where: { id: videoId },
+            data: { transcription }
+        })
+
+        return { transcription }
+    })
+}
